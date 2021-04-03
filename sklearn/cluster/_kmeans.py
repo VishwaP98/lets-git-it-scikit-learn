6b2223a6e2bb09699e7c6f96adc7fd4db2022e88
Ryland Wang <rylandwang@gmail.com>
2021-04-03 17:49:32 -0400

    some small changes

--- sklearn/cluster/_kmeans.py
+++ sklearn/cluster/_kmeans.py
@@ -2025,7 +2025,7 @@ class BisectingKMeans():
         self.max_n_clusters = max_n_clusters
         self.labels = None  # np arrays with the same size as X
         self.centroids = None
-        self.scores = None
+        self.scores = np.zeros(max_n_clusters)
         pass
 
     def fit(self, X):
@@ -2036,46 +2036,48 @@ class BisectingKMeans():
         # check fitted
         # check type of X
         # init
-        labels = np.zeros(X.shape[0])
-        largestLabel = 0
+        self.labels = np.zeros(X.shape[0])
+        self.centroids = np.asarray([[0]])
+        
+        # largestLabel = 0
 
-        for i in range(self.max_n_clusters - 1):
-            kmeans = KMeans(n_clusters=2, init='k-means++')
-            # target_label = self._next_cluster_to_split()
-            target_label = 2
-            # find corresponding points in the target_cluster call sub_X
-            # use labels to create a mask and filter X
+        # for i in range(self.max_n_clusters - 1):
+        #     kmeans = KMeans(n_clusters=2, init='k-means++')
+        #     # target_label = self._next_cluster_to_split()
+        #     target_label = 2
+        #     # find corresponding points in the target_cluster call sub_X
+        #     # use labels to create a mask and filter X
 
-            target_label_indices = np.where(labels == target_label)
+        #     target_label_indices = np.where(labels == target_label)
 
-            # using the target_label_indices get the corresponding X values into sub_X
-            # sub_X = X[target_label_indices]
+        #     # using the target_label_indices get the corresponding X values into sub_X
+        #     # sub_X = X[target_label_indices]
 
-            # kmeans.fit(sub_X)
-            # sub_labels = kmeans.labels_  # np.ndarray with lengh equal to sub_X, labels 0 or 1
-            # score = kmeans.score()
-            # sub_centroids = kmeans.cluster_centers_
+        #     # kmeans.fit(sub_X)
+        #     # sub_labels = kmeans.labels_  # np.ndarray with lengh equal to sub_X, labels 0 or 1
+        #     # score = kmeans.score()
+        #     # sub_centroids = kmeans.cluster_centers_
 
-            # end of this loop[]
+        #     # end of this loop[]
 
-            # kmeans = KMeans(n_clusters=3, random_state=0).fit(X)
-            # print(kmeans.labels_)
-            # print(kmeans.cluster_centers_)
+        #     # kmeans = KMeans(n_clusters=3, random_state=0).fit(X)
+        #     # print(kmeans.labels_)
+        #     # print(kmeans.cluster_centers_)
 
-            # update X with new labels
-            # [0, 1] -> map elements with 0 label to targetLabel and
-            # elements with 1 label to largestLabel+1
-            # update labels using sub_labels
+        #     # update X with new labels
+        #     # [0, 1] -> map elements with 0 label to targetLabel and
+        #     # elements with 1 label to largestLabel+1
+        #     # update labels using sub_labels
 
-            # self._update_labels(sub_labels, target_label_indices, target_label)
-            # self._update_centroids(sub_centroids, target_label)
-            # self._update_scores(sub_scores, target_label)
+        #     # self._update_labels(sub_labels, target_label_indices, target_label)
+        #     # self._update_centroids(sub_centroids, target_label)
+        #     # self._update_scores(sub_scores, target_label)
 
-            # update self.centroids self.scores with new labels and sub_scores
+        #     # update self.centroids self.scores with new labels and sub_scores
 
-            # if early_stopping:
-            #     # check stopping criteria
-            #     break
+        #     # if early_stopping:
+        #     #     # check stopping criteria
+        #     #     break
 
         return self
 
@@ -2119,15 +2121,17 @@ class BisectingKMeans():
         # for each point in X, check which centroid is closest
         for x in X:
             # inertia = sum of squared distance to cluster centroid
-            min_inertia = float('inf')
+            min_cost = float('inf')
             label = 0
 
             for i in range(len(self.centroids)):
                 centroid = self.centroids[i]
-                inertia = self._euclidean_distance(x, centroid)
+                # centroid = [centroid] if isinstance(centroid, float) else centroid
 
-                if inertia < min_inertia:
-                    min_inertia = inertia
+                cost = self._euclidean_distance(x, centroid)
+
+                if cost < min_cost:
+                    min_cost = cost
                     label = i
             
             predictions.append(label)
